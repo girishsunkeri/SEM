f114611e81279ccc60478fcaecd87bd787ed8f9d
girishsunkeri <sunkerigirish@gmail.com>
2015-06-08 01:10:45 +0530

    Added home and categories screen

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('Sem', ['ionic','Sem.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,54 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/main.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: "/home",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/home.html",
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('app.browse', {
+    url: "/browse",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/browse.html"
+      }
+    }
+  })
+    .state('app.categories', {
+      url: "/categories",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/categories.html",
+          controller: 'CategoriesCtrl'
+        }
+      }
+    })
+
+  .state('app.category', {
+    url: "/categories/:categoryId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/category.html",
+        controller: 'CategoryCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,17 @@
+angular.module('Sem.controllers', [])
+
+.controller('AppCtrl', function($scope) {
+	
+})
+
+.controller('HomeCtrl', function($scope){
+
+})
+
+.controller('CategoriesCtrl', function($scope){
+
+})
+
+.controller('CategoryCtrl', function($scope){
+
+})
